a09d1feeee8f5ac255a5d354f1e025ff91d71019
profOlavo <itounip@gmail.com>
2024-12-31 11:05:04 -0300

    Cobrinha Desenvolvimento 00 - Projeto Vazio

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Cobrinha Desenvolvimento 00"
+include(":app")
+ 
