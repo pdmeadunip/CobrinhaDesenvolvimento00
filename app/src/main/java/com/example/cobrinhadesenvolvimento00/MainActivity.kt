7937003229563d5b7c5f0de5dba492bb901d71ce
profOlavo <itounip@gmail.com>
2024-12-31 18:56:09 -0300

    $ direções implantadas, depois dos botões. Botões Ok

--- app/src/main/java/com/example/cobrinhadesenvolvimento00/MainActivity.kt
+++ app/src/main/java/com/example/cobrinhadesenvolvimento00/MainActivity.kt
@@ -24,6 +24,8 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
@@ -41,6 +43,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.Dp
 
 class MainActivity : ComponentActivity() {
@@ -134,15 +137,21 @@ BoxWithConstraints(){
                 modifier = tamBotaoMod,
                 shape = RoundedCornerShape(16.dp)
             ) {
-                Text("Cima")
-            }
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_seta_cima),
+                    contentDescription = "cima"
+                )
+               }
             Row {
                Button(
                     onClick = { game.direcaoAtual = Pair(-1, 0) },
                     modifier = tamBotaoMod,
                     shape = RoundedCornerShape(16.dp)
                ){
-                    Text("Esquerda")
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_seta_esquerda),
+                        contentDescription = "esquerda"
+                    )
                }
                 Spacer(modifier = tamBotaoMod)
                 Button(
@@ -150,7 +159,10 @@ BoxWithConstraints(){
                     modifier = tamBotaoMod,
                     shape = RoundedCornerShape(16.dp)
                 ) {
-                    Text("Direita")
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_seta_direita),
+                        contentDescription = "direita"
+                    )
                 }
             }
             Button(
@@ -158,7 +170,10 @@ BoxWithConstraints(){
                 modifier = tamBotaoMod,
                 shape = RoundedCornerShape(16.dp)
             ) {
-                Text("Baixo")
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_seta_baixo),
+                    contentDescription = "baixo"
+                )
             }
 
         }
